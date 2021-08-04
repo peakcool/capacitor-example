40d67edcb91508fcb29e9b0c17f32bbe1082e4c8
peakcool <tlc852@qq.com>
2021-08-04 22:02:41 +0800

    feat: init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<q-layout view="lHh Lpr lFf">
+		<q-header elevated class="glossy">
+			<q-toolbar>
+				<q-btn
+					flat
+					dense
+					round
+					@click="leftDrawerOpen = !leftDrawerOpen"
+					aria-label="Menu"
+					icon="menu"
+				/>
+
+				<q-toolbar-title> Quasar App </q-toolbar-title>
+
+				<div>Quasar v{{ $q.version }}</div>
+			</q-toolbar>
+		</q-header>
+
+		<q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-2">
+			<q-list>
+				<q-item-label header>Essential Links</q-item-label>
+				<q-item clickable to="/">
+					<q-item-section avatar>
+						<q-icon name="home" />
+					</q-item-section>
+					<q-item-section>
+						<q-item-label>主页</q-item-label>
+						<q-item-label caption>点击返回主页</q-item-label>
+					</q-item-section>
+				</q-item>
+				<q-item clickable to="/network">
+					<q-item-section avatar>
+						<q-icon name="wifi" />
+					</q-item-section>
+					<q-item-section>
+						<q-item-label>网络状态</q-item-label>
+						<q-item-label caption>@capacitor/network</q-item-label>
+					</q-item-section>
+				</q-item>
+				<q-item
+					clickable
+					to="/device"
+				>
+					<q-item-section avatar>
+						<q-icon name="devices" />
+					</q-item-section>
+					<q-item-section>
+						<q-item-label>设备信息</q-item-label>
+						<q-item-label caption>@capacitor/device</q-item-label>
+					</q-item-section>
+				</q-item>
+				<q-item
+					clickable
+					to="/barcode-scanner"
+				>
+					<q-item-section avatar>
+						<q-icon name="qr_code_scanner" />
+					</q-item-section>
+					<q-item-section>
+						<q-item-label>条码扫描</q-item-label>
+						<q-item-label caption>@capacitor-community/barcode-scanner</q-item-label>
+					</q-item-section>
+				</q-item>
+				<q-item
+					clickable
+					to="/status-bar"
+				>
+					<q-item-section avatar>
+						<q-icon name="laptop_mac" />
+					</q-item-section>
+					<q-item-section>
+						<q-item-label>状态栏</q-item-label>
+						<q-item-label caption>@capacitor/status-bar</q-item-label>
+					</q-item-section>
+				</q-item>
+			</q-list>
+		</q-drawer>
+
+		<q-page-container>
+			<router-view />
+			<!-- <HelloWorld /> -->
+		</q-page-container>
+	</q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue';
+// import Network from './components/Network.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'LayoutDefault',
+
+	components: {
+		// HelloWorld,
+		// Network
+	},
+
+	setup() {
+		return {
+			leftDrawerOpen: ref(false)
+		}
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Network.vue
@@ -0,0 +1,12 @@
+<template>
+	<q-page class="flex flex-center"> hello network </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+	name: 'Network'
+}
+</script>
